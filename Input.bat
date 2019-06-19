4b71d51c90336cef94902d385d37af783fa64c23
Mahesh Rasakonda <mahesh.rasakonda@int.whishworks.com>
2019-06-19 16:22:10 +0530

    Add new text

---
+++ Input.bat
@@ -0,0 +1,11 @@
+cd C:\Users\mrasakonda\Desktop\InputData
+::switch to branch you want to use
+git checkout master
+::add all added/modified files
+git add .
+
+git commit -am "Add new text"
+::push to git remote repository
+
+git push --set-upstream "https://github.com/MaheshMahi496/SampleProject.git" master
+Pause
